ab34b3e8d182783734b1e916ad186c68f10e29e0
Mjpelukas <mjpelukas@yahoo.com>
2017-03-06 18:29:21 -0500

    Revert "Deleted Color List"
    This reverts commit 23696cafebc18dfa4a273a29781bcca2f3c90b99.

---
+++ ColorList/ColorList.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+//please make a list of all the colors we would use
+//in the format that follows
+Color( 0, 120, 0);//green
+Color(120, 0, 0);//red
+Color(0, 10, 123);//blue
+Color(0, 40, 200);//purple
+Color(200, 165, 0);//yellow
+Color(130, 100, 0);//orange
+Color(240, 240, 240);//white
+Color(0, 0, 0);//dark-white (grey?)
+}
